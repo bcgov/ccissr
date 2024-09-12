dfb9ba36c7de3ea99a91b06eadf950b4c08b4892
Mahony <Colin.Mahony@gov.bc.ca>
2024-09-12 09:57:39 -0700

    create a data.R file and add Roxygen documentation for the BGCs_notin_THLB.rda

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Biogeoclimatic subzone-variants that are generally considered to be outside the Timber Harvesting Land Base
+#'
+#' @format ## `BGCs_notin_THLB`
+#' A data table with 211 rows and 2 columns:
+#' \describe{
+#'   \item{BGC}{Biogeoclimatic subzone-variant}
+#'   \item{Exclude}{Excluded from THLB}
+#' }
+"BGCs_notin_THLB"
