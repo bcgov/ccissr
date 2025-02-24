ac1a12ca4d0c5ccc61fa830204242f663109a1a9
Comeau <Vanessa.Comeau@gov.bc.ca>
2025-02-24 15:06:17 -0800

    update to script with v13_4

--- R/feasibility_overwrite.R
+++ R/feasibility_overwrite.R
@@ -1,2 +1,2 @@
-feas_update<-read.csv("feas_tables/versioned/Feasibility_v13_3.csv") 
+feas_update<-read.csv("feas_tables/versioned/Feasibility_v13_4.csv") 
 write.csv(feas_update, "feas_tables/feasibility.csv")
