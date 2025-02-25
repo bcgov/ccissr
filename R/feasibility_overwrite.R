9c15372eb489479a7a7805685d33050aa5696001
Courtney Collins <Courtney.Collins@gov.bc.ca>
2025-02-24 17:56:11 -0800

    script to track changes in feasibility/suitability tables and write report

--- R/feasibility_overwrite.R
+++ R/feasibility_overwrite.R
@@ -1,2 +1,26 @@
+library(dplyr)
+library(compareDF)
+
+#read in current table
+feas<-read.csv("feas_tables/feasibility.csv")
+feas$X<-NULL
+names(feas)
+
+#read in updated table
 feas_update<-read.csv("feas_tables/versioned/Feasibility_v13_4.csv") 
+
+#run diff and write out report 
+diff <- compareDF::compare_df(feas_update, feas, group_col = c("ss_nospace", "spp"), stop_on_error = FALSE)
+#diff$change_summary
+#diff$comparison_df
+
+write.csv(diff$comparison_df, "feas_tables/versioned/tracked_changes/diff_report_v13_4.csv") #rename with version to match update 
+
+#overwrite current table with updated table 
+#indicate in git commit 
 write.csv(feas_update, "feas_tables/feasibility.csv")
+
+
+
+
+              
