583c6471d7ee133bf3c4da5b57522c52cbdaf822
Courtney Collins <Courtney.Collins@gov.bc.ca>
2025-02-27 14:35:50 -0800

    feas table v13_6 diff report from coast update

--- R/feasibility_overwrite.R
+++ R/feasibility_overwrite.R
@@ -7,14 +7,15 @@ feas$X<-NULL
 names(feas)
 
 #read in updated table
-feas_update<-read.csv("feas_tables/versioned/Feasibility_v13_4.csv") 
+feas_update<-read.csv("feas_tables/versioned/Feasibility_v13_6.csv") 
+feas_update$X<-NULL
 
 #run diff and write out report 
 diff <- compareDF::compare_df(feas_update, feas, group_col = c("ss_nospace", "spp"), stop_on_error = FALSE)
 #diff$change_summary
-#diff$comparison_df
+diffrept<-diff$comparison_df
 
-write.csv(diff$comparison_df, "feas_tables/versioned/tracked_changes/diff_report_v13_4.csv") #rename with version to match update 
+write.csv(diffrept, "feas_tables/versioned/tracked_changes/diff_report_v13_6.csv") #rename with version to match update 
 
 #overwrite current table with updated table 
 #indicate in git commit 
