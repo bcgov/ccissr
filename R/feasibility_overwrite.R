b0597466aaf78116306c8c701a34fa9d2b776ba0
Courtney Collins <Courtney.Collins@gov.bc.ca>
2025-02-27 14:51:17 -0800

    diff rept feas 13_7- removed wetland units that are no longer valid

--- R/feasibility_overwrite.R
+++ R/feasibility_overwrite.R
@@ -7,15 +7,15 @@ feas$X<-NULL
 names(feas)
 
 #read in updated table
-feas_update<-read.csv("feas_tables/versioned/Feasibility_v13_6.csv") 
+feas_update<-read.csv("feas_tables/versioned/Feasibility_v13_7.csv") 
 feas_update$X<-NULL
 
 #run diff and write out report 
 diff <- compareDF::compare_df(feas_update, feas, group_col = c("ss_nospace", "spp"), stop_on_error = FALSE)
-#diff$change_summary
+diff$change_summary
 diffrept<-diff$comparison_df
 
-write.csv(diffrept, "feas_tables/versioned/tracked_changes/diff_report_v13_6.csv") #rename with version to match update 
+write.csv(diffrept, "feas_tables/versioned/tracked_changes/diff_report_v13_7.csv") #rename with version to match update 
 
 #overwrite current table with updated table 
 #indicate in git commit 
