0c936d544145ed6b07240fca5b7eb76acf7278d3
Courtney Collins <Courtney.Collins@gov.bc.ca>
2025-03-04 16:36:53 -0800

    clean up and reorganize branch for only tables and table updates

--- R/ccissr.R
+++
@@ -1,3 +0,0 @@
-# import compiled code from this package
-#' @useDynLib ccissr, .registration = TRUE
-NULL
--- R/globalVars.R
+++
@@ -1,6 +0,0 @@
-utils::globalVariables(c(
-"BGC", "BGC.prop.y", "CMD", "CMD.def", "CMD.total", "CMD07", "CMDMax", "Cache", "DD_0_at", "DD_0_wt",
-"DD_delayed", "Num", "PERIOD", "PPT.dormant", "PPT05", "PPT06", "PPT07", "PPT08", "PPT09",
-"PPT_JAS", "PPT_MJ", "PPT_at", "PPT_wt", "compareGeom", "crs", "curr", "project", "rmFlag",
-"SppCode", "SppCodePred", "i.SpecialCode",  "."
-))
