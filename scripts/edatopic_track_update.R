50483e1256d3ab7187bd207df03afebe7670e28a
Courtney Collins <Courtney.Collins@gov.bc.ca>
2025-03-18 14:22:29 -0700

    converting SWBun to h/m/x site series

--- scripts/edatopic_track_update.R
+++ scripts/edatopic_track_update.R
@@ -2,25 +2,24 @@ library(dplyr)
 library(compareDF)
 
 #read in current table
-suit<-read.csv("tables/suitability.csv")
-suit$X<-NULL
-names(suit)
+edat<-read.csv("tables/edatopic.csv")
+edat$X<-NULL
+names(edat)
 
 #read in updated table
-suit_update<-read.csv("tables/versioned/suitability_v13_13.csv") 
-suit_update$X<-NULL
+edat_update<-read.csv("tables/versioned/Edatopic_v13_8.csv") 
+edat_update$X<-NULL
 
 #run diff and write out report 
-diff <- compareDF::compare_df(suit_update, suit, group_col = c("ss_nospace", "spp"), stop_on_error = FALSE)
+diff <- compareDF::compare_df(edat_update, edat, group_col = c("SS_NoSpace", "Edatopic"), stop_on_error = FALSE)
 diff$change_summary
 diffrept<-diff$comparison_df
 
-write.csv(diffrept, "tables/versioned/tracked_changes/suitability/diff_report_v13_13.csv") #rename with version to match update 
+write.csv(diffrept, "tables/versioned/tracked_changes/edatopic/diff_report_v13_8.csv") #rename with version to match update 
 
 #overwrite current table with updated table 
 #indicate in git commit 
-write.csv(suit_update, "tables/suitability.csv")
-
+write.csv(edat_update, "tables/edatopic.csv")
 
 #update readme 
 path <- "tables/README.md"
@@ -31,6 +30,3 @@ updated_readme<- c("# Current table versions ",  "Suitability v13_13 ",        "
 # Write the updated content back to the README
 writeLines(updated_readme, path)
 
-
-
-              
