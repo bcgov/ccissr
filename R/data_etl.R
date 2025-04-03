0fb232cd05b1a0d8e62456458940cbb23218db6b
Kiri <kiridaust@gmail.com>
2025-04-03 13:12:37 -0700

    Fix bug in points info

--- R/data_etl.R
+++ R/data_etl.R
@@ -24,10 +24,6 @@
 #' @export
 dbPointInfo <- function(con, points) {
   
-  if (!requireNamespace("sf")) {
-    stop("Please install 'sf' package or provide a data.table or SpatVector to coords.")
-  }
-  
   # It is more efficient for the query optimizer to do
   # each join in a separate sql, better use of index and caching
   # geometry have been optimized (split polygons) for the
@@ -35,39 +31,27 @@ dbPointInfo <- function(con, points) {
   
   Long <- .subset2(points, "Long")
   Lat <- .subset2(points, "Lat")
-  # points[,mls := paste0("(",Long," ",Lat,")")]
-  # wkt_str <- paste0("MULTIPOINT(", paste(pnts$mls, collapse = ", "),")")
+  if(!"ID" %in% colnames(points)){
+    ID <- seq_along(Long)
+  } else {
+    ID <- .subset2(points, "ID")
+  }
   
-  # Some elevation rasters extent overlap. I'm taking the maximum elevation value per points
-  # From the test I've done, they all share the same values.
   elev_info_sql <- paste0("
     WITH pts4269 AS (
-      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 4269) geom", collapse = "\n UNION ALL \n") ,"
+      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 4269) geom,
+      ",ID," id ", collapse = "\n UNION ALL \n") ,"
     )
     
-    SELECT MAX(ROUND(CAST(ST_Value(dem.rast, pts.geom) as NUMERIC), 2)) elevation_m
+    SELECT pts.id, 
+    MAX(ROUND(CAST(ST_Value(dem.rast, pts.geom) as NUMERIC), 2)) elevation_m
     FROM bc_elevation dem
     CROSS JOIN pts4269 pts
     WHERE ST_Intersects(dem.rast, pts.geom)
-    GROUP BY pts
+    GROUP BY pts.id
   ")
   
-  
-  
-  # bec_subz_sql <- paste0("
-  #   WITH pts4326 AS (
-  #     ", paste0("SELECT st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326) geom", collapse = "\n UNION ALL \n") ,"
-  #   )
-  #   
-  #   SELECT bgc_map.BGC
-  #   FROM pts4326
-  #   LEFT JOIN bgc_map
-  #   ON ST_Intersects(pts4326.geom, bgc_map.geom)
-  #                        ")
-  
-  if(nrow(points) > 1){
-    ID <- .subset2(points, "ID")
-    bec_info_sql <- paste0("
+  bec_info_sql <- paste0("
     WITH pts3005 AS (
       ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom,
       ",ID," id ", collapse = "\n UNION ALL \n") ,"
@@ -92,42 +76,15 @@ dbPointInfo <- function(con, points) {
     LEFT JOIN bec_info bec
     ON ST_Intersects(pts.geom, bec.geometry)
   ")
-    
-  }else{
-    bec_info_sql <- paste0("
-    WITH pts3005 AS (
-      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom ", collapse = "\n UNION ALL \n") ,"
-    )
-    
-    SELECT bec.zone,
-           bec.subzone,
-           bec.variant,
-           bec.phase,
-           bec.natural_disturbance,
-           bec.map_label,
-           bec.bgc_label,
-           bec.zone_name,
-           bec.subzone_name,
-           bec.variant_name,
-           bec.phase_name,
-           bec.natural_disturbance_name,
-           bec.feature_area_sqm,
-           bec.feature_length_m
-    FROM pts3005 pts
-    LEFT JOIN bec_info bec
-    ON ST_Intersects(pts.geom, bec.geometry)
-  ")
-  }
-  
-  
-  #test <- dbGetQuery(pool, bec_info_sql)
   
   site_ref_sql <- paste0("
     WITH pts3005 AS (
-      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom", collapse = "\n UNION ALL \n") ,"
+      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom,
+      ",ID," id ", collapse = "\n UNION ALL \n") ,"
     )
     
-    SELECT cast(hgrid.siteno as text) site_no
+    SELECT pts.id, 
+    hgrid.siteno site_no
     FROM pts3005 pts 
     LEFT JOIN hex_grid hgrid
     ON ST_Intersects(pts.geom, hgrid.geom)
@@ -135,10 +92,12 @@ dbPointInfo <- function(con, points) {
   
   bc_land_sql <- paste0("
     WITH pts3005 AS (
-      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom", collapse = "\n UNION ALL \n") ,"
+      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom,
+      ",ID," id ", collapse = "\n UNION ALL \n") ,"
     )
     
-    SELECT bcb_hres.objectid is NOT NULL onbcland
+    SELECT pts.id,
+    bcb_hres.objectid is NOT NULL onbcland
     FROM pts3005 pts
     LEFT JOIN bcb_hres
     ON ST_Within(pts.geom, bcb_hres.geometry)
@@ -146,23 +105,31 @@ dbPointInfo <- function(con, points) {
   
   bc_forest_region <- paste0("
     WITH pts3005 AS (
-      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom", collapse = "\n UNION ALL \n") ,"
+      ", paste0("SELECT st_transform(st_pointfromtext('POINT(", Long, " ", Lat, ")', 4326), 3005) geom,
+      ",ID," id ", collapse = "\n UNION ALL \n") ,"
     )
     
-    SELECT region_ref forest_region
+    SELECT pts.id,
+    region_ref forest_region
     FROM pts3005 pts
     LEFT JOIN bc_forest_regions
     ON ST_Within(pts.geom, bc_forest_regions.geom)                       
   ")
   
-  res1 <- setDT(RPostgres::dbGetQuery(con, bec_info_sql))
-  res2 <- setDT(RPostgres::dbGetQuery(con, elev_info_sql))
-  res3 <- setDT(RPostgres::dbGetQuery(con, site_ref_sql))
-  res4 <- setDT(RPostgres::dbGetQuery(con, bc_land_sql))
-  res5 <- setDT(RPostgres::dbGetQuery(con, bc_forest_region))
-  
-  return(cbind(res1, res2, res3, res4, res5))  
+  res1 <- RPostgres::dbGetQuery(con, bec_info_sql) |> as.data.table()
+  setkey(res1,id)
+  res2 <- RPostgres::dbGetQuery(con, elev_info_sql)|> as.data.table()
+  setkey(res2,id)
+  res3 <- RPostgres::dbGetQuery(con, site_ref_sql)|> as.data.table()
+  setkey(res3,id)
+  res4 <- RPostgres::dbGetQuery(con, bc_land_sql)|> as.data.table()
+  setkey(res4,id)
+  res5 <- RPostgres::dbGetQuery(con, bc_forest_region)|> as.data.table()
+  setkey(res5,id)
+  res <- res1[res2,][res3,][res4,][res5,]
+  return(res)  
 }
+
 
 # Use the database, it is faster than R native sf functions
 # The buffer is to make sure points are not at the edge of the map
