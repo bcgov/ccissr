28a217dbc7d83afc61dbd9a1ca4a5f88639d0f26
Courtney Collins <Courtney.Collins@gov.bc.ca>
2025-04-07 15:39:59 -0700

    script to look at suitability ratings across bgcs for broadleaf spp

---
+++ scripts/regional_updates/broadleaf_dist.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+#read in current table
+suit<-read.csv("tables/suitability.csv")
+suit$X<-NULL
+sort(unique(suit$spp))
+
+broads<-subset(suit, spp=="At"|spp=="Act"|spp=="Ac"|spp=="Ep"|spp=="Dr"|spp=="Mb"|
+                 spp=="Ra"|spp=="Wt"|spp=="Mv"|spp=="Ws"|spp=="Wp"|spp=="Qg"|spp=="Gp"|spp=="Vb"|
+                 spp=="Up"|spp=="Kc"| spp=="Vp")
+#remove US units 
+broads<-filter(broads, !grepl('_CA|_OR|_WA|_ID|_MT|_CA|_WY|_CO|_NV|UT|BSJP|abE|abN|abS|abC|SBAP|SASbo', ss_nospace))
+
+broads<-mutate(broads, zone=(case_when(grepl("SWB", bgc)~"SWB",
+                                 grepl("BWBS", bgc)~"BWBS",
+                                 grepl("SBPS", bgc)~"SBPS", 
+                                 grepl("SBS", bgc)~"SBS",
+                                 grepl("MH", bgc)~"MH",
+                                 grepl("ESSF", bgc)~"ESSF",
+                                 grepl("MS", bgc)~"MS",
+                                 grepl("BG", bgc)~"BG",
+                                 grepl("PP", bgc)~"PP",
+                                 grepl("IDF", bgc)~"IDF",
+                                 grepl("CDF", bgc)~"CDF",
+                                 grepl("ICH", bgc)~"ICH",
+                                 grepl("CWH", bgc)~"CWH",TRUE~NA)))
+               
+               
+broads_tab<-group_by(broads, sppsplit, zone)%>%summarise(count=n())
+broads_tab2<-group_by(broads, sppsplit)%>%mutate(count2=n())%>%select(sppsplit, count2)%>%distinct(.)
+broads_tab<-left_join(broads_tab, broads_tab2)
+
+write.csv(broads_tab, "broadleaf_table.csv")
