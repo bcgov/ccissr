7b697e27c41b7636c41f87efe1454a7823463416
Courtney Collins <Courtney.Collins@gov.bc.ca>
2025-04-14 14:24:28 -0700

    TO updates corrected

--- scripts/regional_updates/ThompsonOkanagan_update.R
+++ scripts/regional_updates/ThompsonOkanagan_update.R
@@ -38,21 +38,21 @@ suitx<- mutate(suitx,
   newsuit = if_else(is.na(newsuit) & !is.na(action_numeric), action_numeric, newsuit)) %>%
   select(-action_numeric) 
 
-blanks<-subset(suitx, is.na(newsuit)& is.na(Action))
-sort(unique(blanks$ss_nospace)) #check remaining blanks-> mostly non forested units 
-
 #calculate change stats  
-suitx<-mutate(suitx, feas_change= if_else(newsuit==newsuit1, 0, 1))
+suitx<-mutate(suitx, feas_change= if_else(newsuit==newsuit1, 0, 1))%>%
+  mutate(feas_change=if_else(is.na(feas_change), 0, feas_change))
 
 #percent change 
 nchange<-sum(suitx$feas_change == 1, na.rm = TRUE)
 nsame<-sum(suitx$feas_change == 0, na.rm = TRUE)
-nblank<-sum(is.na(suitx$feas_change))
 total<-nrow(suitx)-nblank
 nrow(new)
 
-round(nchange/total, 2)*100
+round(nchange/total, 2)*100 
 round(nrow(new)/total, 2)*100
+
+blanks<-subset(suitx, is.na(newsuit)& is.na(Action))
+sort(unique(blanks$ss_nospace)) #check remaining blanks-> mostly non forested units 
 
 #clean up 
 suitxy<-select(suitx,bgc, ss_nospace, spp, newsuit, mod, outrange)
@@ -62,6 +62,12 @@ TO_update2<-rbind(suitxy, new)%>%mutate(outrange=if_else(spp=="Lw", TRUE, FALSE)
 TO_update3<-filter(TO_update2, bgc=='ESSFdh2' & is.na(mod))
 TO_update2<-anti_join(TO_update2, TO_update3)  
 TO_update2<-subset(TO_update2, ss_nospace!="ESSFdc3/04")
+
+#keep previous non-forested unit ratings 
+blanks<-select(blanks, bgc, ss_nospace, spp, newsuit1, mod1)
+TO_update4<-left_join(TO_update2, blanks)
+TO_update2<-mutate(TO_update4, newsuit=if_else(is.na(newsuit),newsuit1, newsuit))%>%
+  select(-newsuit1, -mod1)
 
 #pull out ratings of bgcs in TO update from main  
 suitx<-subset(suit, bgc %in% TO_update$bgc)
@@ -76,3 +82,5 @@ suit<-rbind(suit, TO_update2)
 
 #write as new version
 write.csv(suit, "tables/versioned/Suitability_v13_15.csv")
+
+
