aa7903c3e85ba03280428316e9670ea154011398
Courtney Collins <courtneygrace125@gmail.com>
2025-05-20 17:52:42 -0700

    suitability v13_19 track update

--- scripts/suitability_track_update.R
+++ scripts/suitability_track_update.R
@@ -7,7 +7,7 @@ suit$X<-NULL
 names(suit)
 
 #read in updated table
-suit_update<-read.csv("tables/versioned/suitability_v13_18.csv") 
+suit_update<-read.csv("tables/versioned/suitability_v13_19.csv") 
 suit_update$X<-NULL
 
 #run diff and write out report 
@@ -15,7 +15,7 @@ diff <- compareDF::compare_df(suit_update, suit, group_col = c("ss_nospace", "sp
 diff$change_summary
 diffrept<-diff$comparison_df
 
-write.csv(diffrept, "tables/versioned/tracked_changes/suitability/diff_report_v13_18.csv") #rename with version to match update 
+write.csv(diffrept, "tables/versioned/tracked_changes/suitability/diff_report_v13_19.csv") #rename with version to match update 
 
 #overwrite current table with updated table 
 #indicate in git commit 
@@ -24,7 +24,7 @@ write.csv(suit_update, "tables/suitability.csv")
 
 #update readme 
 path <- "tables/README.md"
-updated_readme<- c("# Current table versions ",  "Suitability v13_18 ",        ""               ,
+updated_readme<- c("# Current table versions ",  "Suitability v13_19 ",        ""               ,
                    "Edatopic v13_11 ",    ""               ,
                    "WNA BGCs v13_2 ",     ""               ,
                    "Site series v13_2")     #update version #s 
