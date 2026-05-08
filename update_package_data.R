379828b4609c0e62482d053446de5c978d6cce14
Kiri <kiridaust@gmail.com>
2026-05-08 16:50:16 -0700

    Updated data tables

--- update_package_data.R
+++ update_package_data.R
@@ -8,8 +8,9 @@ S1[,V1 := NULL]
 setnames(S1, old = c("suitability","newsuit"), new = c("feasible","newfeas"))
 
 E1 <- fread("https://raw.githubusercontent.com/bcgov/ccissr/refs/heads/feas_tables/tables/edatopic.csv", header = TRUE)
-SS <- fread("https://raw.githubusercontent.com/bcgov/ccissr/refs/heads/feas_tables/tables/site_series.csv")
-SS <- SS[,.(SS_NoSpace,SpecialCode)]
+SS <- fread("https://raw.githubusercontent.com/bcgov/ccissr/refs/heads/feas_tables/tables/site_series.csv", header = TRUE, encoding = "Latin-1")
+SS[SpecialCode == "", SpecialCode := NA]
+SS <- SS[,.(SS_NoSpace,SpecialCode,SiteSeriesLongName)]
 SS <- unique(SS)
 E1[SS, SpecialCode := i.SpecialCode, on = "SS_NoSpace"]
 
@@ -22,4 +23,4 @@ E1_Phase <- rbind(phases,vars)
 E1_Phase[,MainUnit := gsub("[a-z]$","",SS_NoSpace)]
 E1_Phase[,MainUnit := gsub("\\.[1-9]$","",MainUnit)]
 E1[,V1 := NULL]
-use_data(S1, E1, E1_Phase, overwrite = TRUE)
+use_data(S1, E1, E1_Phase, SS, overwrite = TRUE)
